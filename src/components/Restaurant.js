2e7190fa16ac7c9bc6df8686fd832eea0a167eff
Donghun Won <wonhun1225@gmail.com>
2024-01-16 20:33:36 +0900

    feat(component) : restaurant 컴포넌트

---
+++ src/components/Restaurant.js
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '../css/Restaurant.module.css';
+
+export default function Restaurant({ data, category }) {
+  const navigate = useNavigate();
+
+  let Restaurant = data.map((data) => {
+    return (
+      <>
+        <div
+          className={`col text-center`}
+          onClick={() =>
+            navigate(`/category/${category}/restaurant/${data.name}`)
+          }
+        >
+          <div className={`${styles.menuWrapper}`}>
+            <span className={`material-symbols-outlined ${styles.textlarge}`}>
+              {data.icon}
+            </span>
+            <span className={`${styles.name}`}>{data.name}</span>
+          </div>
+        </div>
+        <div className="w-100"></div>
+      </>
+    );
+  });
+  return <>{Restaurant}</>;
+}
