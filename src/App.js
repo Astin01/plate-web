7b95e25b95df020909de28d30f001c12663bf58c
Donghun Won <wonhun1225@gmail.com>
2024-02-01 20:52:41 +0900

    refactor : router에서 pages로 폴더 변경

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import SiteNav from './components/SiteNav';
 
 import styles from './css/App.module.css';
-import Home from './router/Home';
-import Login from './router/Login';
-import SignIn from './router/SignIn';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignIn from './pages/SignIn';
 import React, { Suspense, useState } from 'react';
-import EventDetail from './router/EventDetail';
-import Mypage from './router/Mypage';
+import EventDetail from './pages/EventDetail';
+import Mypage from './pages/Mypage';
+import Admin from './pages/Admin';
 
 function App() {
   const [isLogin, setIsLogin] = useState(0);
   const Category = React.lazy(() => import('./components/Category'));
-  const Restaurant = React.lazy(() => import('./router/Restaurant'));
-  const RestaurantDetail = React.lazy(() =>
-    import('./router/RestaurantDetail')
-  );
+  const Restaurant = React.lazy(() => import('./pages/Restaurant'));
+  const RestaurantDetail = React.lazy(() => import('./pages/RestaurantDetail'));
   return (
     <>
       <div className={`${styles.entireWrap}`}>
@@ -25,10 +24,7 @@ function App() {
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route
-                path="/login"
-                element={<Login setIsLogin={setIsLogin} />}
-              />
+              <Route path="/login" element={<Login />} />
               <Route path="/signin" element={<SignIn />} />
               <Route path="/event/:id" element={<EventDetail />} />
               <Route path="/category/:category" element={<Category />} />
@@ -41,6 +37,7 @@ function App() {
                 element={<RestaurantDetail />}
               />
               <Route path="/mypage" element={<Mypage />} />
+              <Route path="/admin" element={<Admin />} />
             </Routes>
           </Suspense>
         </div>
