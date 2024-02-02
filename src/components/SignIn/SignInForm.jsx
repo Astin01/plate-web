f01ed637449305e5f7ca62d91f5a1aaabdf2674f
Donghun Won <wonhun1225@gmail.com>
2024-02-02 15:57:42 +0900

    feat(component) : 회원가입 컴포넌트

---
+++ src/components/SignIn/SignInForm.jsx
@@ -0,0 +1,64 @@
+import { Button, Container, Form, Row } from 'react-bootstrap';
+import styles from './SignInForm.module.css';
+
+export default function SignInForm({ signIn }) {
+  const onSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userId = form.userId.value;
+    const userPassword = form.userPassword.value;
+    const name = form.name.value;
+    const nickname = form.nickname.value;
+    const email = form.email.value;
+    console.log('userId : ' + userId);
+    console.log('userPassword : ' + userPassword);
+    console.log('name : ' + name);
+
+    signIn({ userId, userPassword, name, nickname, email });
+  };
+  return (
+    <Container className={styles.container}>
+      <Form
+        onSubmit={(e) => {
+          onSignIn(e);
+        }}
+      >
+        <Row>
+          <Form.Group className="mb-3" controlId="userId">
+            <Form.Label>아이디</Form.Label>
+            <Form.Control type="text" placeholder="id" />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group className="mb-3" controlId="userPassword">
+            <Form.Label>비밀번호</Form.Label>
+            <Form.Control type="password" placeholder="password" />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>이름</Form.Label>
+            <Form.Control type="text" placeholder="username" />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group className="mb-3" controlId="nickname">
+            <Form.Label>닉네임</Form.Label>
+            <Form.Control type="text" placeholder="nickname" />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>이메일</Form.Label>
+            <Form.Control type="email" placeholder="email" />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Button variant="primary" type="submit">
+            회원가입
+          </Button>
+        </Row>
+      </Form>
+    </Container>
+  );
+}
