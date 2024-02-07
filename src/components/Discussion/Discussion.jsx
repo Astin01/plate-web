5495f0a18bacaabe02ddc7338f080311895e305c
Donghun Won <wonhun1225@gmail.com>
2024-02-07 17:18:15 +0900

    feat(component) : discussion

---
+++ src/components/Discussion/Discussion.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '../../css/Discussion/Discussion.module.css';
+
+const Discussion = ({ data }) => {
+  const navigate = useNavigate();
+
+  let Discussion = data.map((data) => {
+    return (
+      <>
+        <div
+          className={`col text-center`}
+          onClick={() => navigate(`/discussion/${data.id}`)}
+        >
+          <div className={`${styles.menuWrapper}`}>
+            <span className={`${styles.name}`}>{data.title}</span>
+          </div>
+        </div>
+        <div className="w-100"></div>
+      </>
+    );
+  });
+  return <>{Discussion}</>;
+};
+
+export default Discussion;
