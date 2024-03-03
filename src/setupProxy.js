f4ea04e8e4296f826daa5875f8c48567937dc5cd
Donghun Won <wonhun1225@gmail.com>
2024-03-03 17:45:10 +0900

    feat(proxy) : proxy 경로 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,15 +4,30 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://ec2-3-38-227-81.ap-northeast-2.compute.amazonaws.com',
       changeOrigin: true,
     })
   );
   app.use(
     '/login',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://ec2-3-38-227-81.ap-northeast-2.compute.amazonaws.com',
       changeOrigin: true,
     })
   );
+
+  // app.use(
+  //   '/api',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:8080',
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   '/login',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:8080',
+  //     changeOrigin: true,
+  //   })
+  // );
 };
