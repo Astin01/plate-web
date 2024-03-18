f5e7a165ed921cc560ed2b835429d13d7a60e618
Donghun Won <wonhun1225@gmail.com>
2024-03-18 13:08:56 +0900

    fix : 탄력적 ip로 주소 변경

--- src/apis/api.js
+++ src/apis/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 const ROOT_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://ec2-3-38-94-43.ap-northeast-2.compute.amazonaws.com'
+    ? 'http://ec2-3-38-212-107.ap-northeast-2.compute.amazonaws.com'
     : '';
 const api = axios.create({
   // baseURL: `${PROXY}`,
