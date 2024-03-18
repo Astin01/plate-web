8ea663408bd80a156cfb4838c6d398a371327cbd
Donghun Won <wonhun1225@gmail.com>
2024-03-18 13:00:56 +0900

    feat : 배포환경에서 cors문제 해결을 위한 proxy 설정

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const ROOT_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://ec2-3-38-94-43.ap-northeast-2.compute.amazonaws.com'
+    : '';
 const api = axios.create({
-  baseURL: `${PROXY}`,
+  // baseURL: `${PROXY}`,
+  baseURL: `${ROOT_URL}`,
 });
 
 export default api;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://ec2-3-38-227-81.ap-northeast-2.compute.amazonaws.com',
+      target: 'http://ec2-3-38-94-43.ap-northeast-2.compute.amazonaws.com',
       changeOrigin: true,
     })
   );
   app.use(
     '/login',
     createProxyMiddleware({
-      target: 'http://ec2-3-38-227-81.ap-northeast-2.compute.amazonaws.com',
+      target: 'http://ec2-3-38-94-43.ap-northeast-2.compute.amazonaws.com',
       changeOrigin: true,
     })
   );
