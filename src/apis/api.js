646c223f5f5f0dbbfcc7f165e9555c237f7ca1e2
Donghun Won <wonhun1225@gmail.com>
2024-03-19 18:37:50 +0900

    fix : netlify proxy 설정

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
-const ROOT_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'http://ec2-3-38-212-107.ap-northeast-2.compute.amazonaws.com'
-    : '';
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+// const ROOT_URL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'http://ec2-3-38-212-107.ap-northeast-2.compute.amazonaws.com'
+//     : '';
 const api = axios.create({
-  // baseURL: `${PROXY}`,
-  baseURL: `${ROOT_URL}`,
+  baseURL: `${PROXY}`,
+  // baseURL: `${ROOT_URL}`,
 });
 
 export default api;
